b5d315e355575adeb97a94c6f919fbe0dc34621f
Ryan Freckleton <ryan.freckleton@gmail.com>
2015-07-29 16:46:01 -0600

    Updating tool with parser and styles.

--- graph.py
+++ graph.py
@@ -2,73 +2,63 @@ import collections
 import textwrap
 import sys
 import pygraphviz
+import parser
 
 import toolz
 import pyrsistent
+import json
+import string
+
+from pprint import pprint
 
 
-Entity = collections.namedtuple("Entity", "id label")
-islabel_line = lambda s: '.' in s
+Entity = collections.namedtuple("Entity", "id label cls")
 
-class Node(pyrsistent.PClass):
-    attributes = pyrsistent.field()
-    children = pyrsistent.field()
+attrs = {
+        None : dict(),
+        'inj' : dict(style="rounded,filled"),
+        'obs' : dict(shape="plaintext"),
+        'and' : dict(shape="ellipse")
+      }
 
-    def to_graphviz(self):
-        raise NotImplementedError
+class Semantics(object):
+    def __init__(self):
+        self.nodes = set()
+        self.edges = set()
+        self.letters = list(string.uppercase)
 
-class Groups(object):
-    def __init__(self, f):
-        self.groups = toolz.groupby(islabel_line, f)
+    def label(self, ast):
+        self.nodes.add(Entity(ast.id, ast.label, ast.cls))
 
-    def label_lines(self):
-        return self.groups[True]
-
-    def edge_lines(self):
-        return self.groups[False]
-
-def get_nodes(edge_lines):
-    nodes = []
-    for el in edge_lines:
-        if el.strip():
-            nodes.append(el.strip().split('->'))
-    return nodes
-
-def get_edges(nodes):
-    edges = []
-    for n in nodes:
-        edges.extend(zip(map(str.strip, n), map(str.strip, n[1:])))
-    return edges
+    def relation(self, ast):
+        if len(ast.source) == 1:
+            self.edges.add((ast.source[0], ast.destination[0]))
+        else:
+            letter = self.letters.pop(0)
+            self.nodes.add(Entity(letter, "AND", "and"))
+            for s in ast.source:
+                self.edges.add((s, letter))
+            self.edges.add((letter, ast.destination[0]))
 
 # imperative
 def main(f):
-    groups = Groups(f)
-    nodes = get_nodes(groups.edge_lines())
-    edges = get_edges(nodes)
-    labels = get_labels(groups.label_lines())
-    g = create_graph(labels, edges)
+    p = parser.thinkingprocessesParser()
+    semantic_graph = Semantics()
+    ast = p.parse(f.read(), rule_name="statements", semantics = semantic_graph)
+    g = create_graph(semantic_graph.nodes, semantic_graph.edges)
     output(g)
 
-
-def get_labels(lines):
-    return (get_entity(line) for line in lines)
 
 # imperative
 def output(g):
     print g
 
 # pure
-def get_entity(line):
-    identifier, label = line.split('.', 1)
-    label = textwrap.fill(label.strip(), width=30)
-    return Entity(identifier, label)
-
-# pure
 def create_graph(labels, edges):
     g = pygraphviz.AGraph(directed=True, rankdir="TB")
     g.node_attr['shape'] = 'rectangle'
-    for identifier, label in labels:
-        g.add_node(identifier, label=label)
+    for identifier, label, cls in labels:
+        g.add_node(identifier, label=label, **attrs[cls])
     g.add_edges_from(edges)
     return g.to_string()
 
